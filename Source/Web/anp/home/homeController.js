24c6b6e4c99ca55a3b63dae810a7ca5b25f38c7c
germanl18 <germanlopezz16@hotmail.com>
2018-10-01 16:55:34 -0300

    Agregamos la vista de como se veran las areas

--- Source/Web/anp/home/homeController.js
+++ Source/Web/anp/home/homeController.js
@@ -1,5 +1,17 @@
 'use strict';
 
 angular.module('Home')
-    .controller('home.anpList', ['$scope',
-    ]);
+    .controller('home.anpList', ['$scope','$mdDialog',
+    function ($scope, $mdDialog) {
+      $scope.mostrarAreaNaturalProtegida = function(event) {
+        $mdDialog.show(
+          $mdDialog.alert()
+          .title('ANP')
+          .textContent('Primary actions can be used for one click actions')
+          .ariaLabel('Primary click demo')
+          .ok('Awesome!')
+          .targetEvent(event)
+        );
+      };
+    }
+  ]);
