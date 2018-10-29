b667a07e7ca4721884e293d6d4223e385067ecd9
fede <federicodiaz545@gmail.com>
2018-10-29 18:30:10 -0300

    Integracion de firebase al fin!

--- Source/ANPWeb/src/app/anp-list/anp-list.component.ts
+++ Source/ANPWeb/src/app/anp-list/anp-list.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-anp-list',
   templateUrl: './anp-list.component.html',
   styleUrls: ['./anp-list.component.css']
 })
-export class AnpListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class AnpListComponent{
+  areas : Observable<any[]>;
+  constructor(db: AngularFireDatabase) {
+    this.areas = db.list('AreasNaturalesProtegidas').valueChanges();     
   }
-
 }
--- Source/ANPWeb/src/app/app.module.ts
+++ Source/ANPWeb/src/app/app.module.ts
@@ -1,22 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { AnpListComponent } from './anp-list/anp-list.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AnpComponent } from './anp/anp.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+var config = {
+  apiKey: "AIzaSyAjoNbn9JeUPcoErpk35J9QAFz40vfno3c",
+  authDomain: "apn-dev-95587.firebaseapp.com",
+  databaseURL: "https://apn-dev-95587.firebaseio.com",
+  projectId: "apn-dev-95587",
+  storageBucket: "apn-dev-95587.appspot.com",
+  messagingSenderId: "645309354612"
+};
+
 
 @NgModule({
   declarations: [
     AppComponent,
     AnpListComponent,
-    AnpComponent
+    AnpComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(config), // imports firebase/app needed for everything
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
+    AngularFireStorageModule, // imports firebase/storage only needed for storage features
+    AngularFireDatabaseModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
