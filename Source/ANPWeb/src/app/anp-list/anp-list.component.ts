2d39a9bc0e3270b3ac7b79720209b3ed9f745e06
SolStrapko <solstrapko@hotmail.com>
2018-11-01 18:58:21 -0300

    Listamos y dimos estilos(que no funcionan).

--- Source/ANPWeb/src/app/anp-list/anp-list.component.ts
+++ Source/ANPWeb/src/app/anp-list/anp-list.component.ts
@@ -1,15 +1,40 @@
-import { Component} from '@angular/core';
+import { Component, ViewChild, OnInit} from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { Observable } from 'rxjs';
+import {animate, state, style, transition, trigger} from '@angular/animations';
+import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-anp-list',
   templateUrl: './anp-list.component.html',
-  styleUrls: ['./anp-list.component.css']
+  styleUrls: ['./anp-list.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0', display: 'none'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ])
+  ]
 })
-export class AnpListComponent{
+export class AnpListComponent implements OnInit {
   areas : Observable<any[]>;
+  columnsToDisplay = ['Nombre', 'Ecorregion'];
+  expandedElement: any;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
   constructor(db: AngularFireDatabase) {
     this.areas = db.list('AreasNaturalesProtegidas').valueChanges();     
   }
+
+  ngOnInit() {
+    /*this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;*/
+  }
+
+  /*applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+        this.dataSource.paginator.firstPage();
+    }*/
 }
--- Source/ANPWeb/src/app/app.module.ts
+++ Source/ANPWeb/src/app/app.module.ts
@@ -9,7 +9,10 @@ import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireAuthModule } from '@angular/fire/auth';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatTableModule} from '@angular/material/table';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
 var config = {
   apiKey: "AIzaSyAjoNbn9JeUPcoErpk35J9QAFz40vfno3c",
   authDomain: "apn-dev-95587.firebaseapp.com",
@@ -28,13 +31,18 @@ var config = {
     
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(config), // imports firebase/app needed for everything
     AngularFirestoreModule, // imports firebase/firestore, only needed for database features
     AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
     AngularFireStorageModule, // imports firebase/storage only needed for storage features
-    AngularFireDatabaseModule
+    AngularFireDatabaseModule,
+    MatTableModule,
+    FormsModule, 
+    ReactiveFormsModule,
+    MatFormFieldModule
   ],
   bootstrap: [AppComponent]
 })
