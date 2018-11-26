cee3c8e05b9e48b938865a4f5fb6f0f6919e6cba
AnaMariaRomero <anneromeroflcl@gmail.com>
2018-11-26 18:43:24 -0300

    Agregamos graficos.

--- Source/ANPWeb/src/app/graphics/graphics.component.ts
+++ Source/ANPWeb/src/app/graphics/graphics.component.ts
@@ -7,16 +7,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GraphicsComponent implements OnInit {
 
-  chart = {
-    title: 'Test Chart',
-    type: 'BarChart',
+  chartEcorregion = {
+    title: 'Ecorregiones',
+    type: 'ColumnChart',
     data: [
-      ['Pampa', 26],
-      ['Delta e Islas del Paraná', 8],
-      ['Pampa y Delta e Islas del Paraná', 12],
+      ['Pampa',26],
+      ['Delta e Islas del Paraná',8],
+      ['Pampa y Delta e Islas del Paraná' , 12],
     ],
-    columnNames: ['Element', 'Density'],
+    columnNames: ['Ecorregión', 'Cantidad de areas por ecorregión'],
     options: {
+      colors: ['#e0440e', '#e6693e'],
+      is3D: true,
+      animation: {
+        duration: 250,
+        easing: 'ease-in-out',
+        startup: true
+      }
+    }
+  };
+  chartCategoriaInternacional = {
+    title: 'Categoría Iternacional',
+    type: 'ColumnChart',
+    data: [
+      ['Reserva de Biosfera', 2],
+      ['AICA', 2],
+      ['Sitio RAMSAR y AICA', 2],
+      ['No posee', 40],
+    ],
+    columnNames: ['Categoria Internacional', 'Cantidad'],
+    options: {
+      colors: ['#e0440e', '#e6693e', '#ec8f6e'],
+      is3D: true,
+      animation: {
+        duration: 250,
+        easing: 'ease-in-out',
+        startup: true
+      }
+    }
+  };
+  chartPoseeAdmin = {
+    title: 'Administraciones',
+    type: 'ColumnChart',
+    data: [
+      ['Nacional', 1],
+      ['Provincial', 10],
+      ['Municipal', 22],
+      ['Privada', 6],
+      ['Mixta', 7],
+    ],
+    columnNames: ['Administración', 'Superficie'],
+    options: {
+      colors: ['#e0440e', '#e6693e', '#ec8f6e'],
+      is3D: true,
       animation: {
         duration: 250,
         easing: 'ease-in-out',
