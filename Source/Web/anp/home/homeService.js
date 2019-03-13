a247822a99c82e78b660fc3566acdc9853c6cf1b
janeko23 <munozjeanette23@gmail.com>
2019-03-13 19:04:17 -0300

    #3 Eliminar la version antigua

--- Source/Web/anp/home/homeService.js
+++
@@ -1,9 +0,0 @@
-angular.module('Home').factory('homeService', ['dataService',
-    function (dataService) {
-        return {
-            getAreasNaturalesProtegidas: function(callback){
-                dataService.getData('AreasNaturalesProtegidas', callback);
-            }
-        };
-    }
-]);
--- Source/Web/anp/layout/layout.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-
-angular.module('Layout',['Core']);
