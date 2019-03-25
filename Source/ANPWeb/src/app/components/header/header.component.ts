a47ffd3b85a8e4a2114d70bc4e77377c1766a049
janeko23 <munozjeanette23@gmail.com>
2019-03-25 18:31:33 -0300

    Agregado de efecto scroll

--- Source/ANPWeb/src/app/components/header/header.component.ts
+++ Source/ANPWeb/src/app/components/header/header.component.ts
@@ -1,12 +1,17 @@
 // src/app/components/header.component.ts
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
+
 export class HeaderComponent implements OnInit {
-  constructor() {}
+  navigate(route){
+    window.scrollTo({top:620, behavior: 'smooth'});
+    this.router.navigate([route]);
+    }
+  constructor(private router : Router) {}
   ngOnInit() {}
-}
+  }
