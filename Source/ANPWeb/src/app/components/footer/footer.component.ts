a7bb540e318e6d27777f2e28fb02ea53e65a39a5
AnaMariaRomero <anneromeroflcl@gmail.com>
2019-03-28 18:28:31 -0300

    Arreglo de footer

--- Source/ANPWeb/src/app/components/footer/footer.component.ts
+++ Source/ANPWeb/src/app/components/footer/footer.component.ts
@@ -1,12 +1,17 @@
 // src/app/components/footer.component.ts
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-footer',
   templateUrl:"./footer.component.html",
   styleUrls: ["./footer.component.css"]
 })
 export class FooterComponent implements OnInit {
-  constructor() {}
+  
+  today: number = Date.now();
+  constructor() {  
+  }
   ngOnInit() {}
 }
+
